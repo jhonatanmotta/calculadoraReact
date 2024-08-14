10fc31b358b9e58feb9cea7939c40b5051617795
jhonatanmotta <jhonatanmotta07@gmail.com>
2024-08-13 22:57:26 -0500

    chore(app): se crearon los estilos y se creo la esturctura basica de la calculadora

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
 
 function App() {
   return (
     <div className="App">
-      Hola
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp'
+        />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
